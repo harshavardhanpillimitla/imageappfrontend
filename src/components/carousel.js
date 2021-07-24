96a4a061cfe1f9ff910877e1069ea983285a3e26
harsha vardhan pillimitla <harshavardhanpillimitla143@gmail.com>
2021-07-24 23:46:29 +0530

    bug fix: image not rotating due to browser cache issue fixed

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 class Carousel extends Component {
   render(){
       return <div className={this.props.active} >
-      <img className="d-block w-100"  id={this.props.id} src={this.props.image}  width="100" height="300"/>
+      <img className="d-block w-100"   id={this.props.id} src={`${this.props.image}?${new Date().getTime()}`}  width="100" height="300"/>
       </div>
       
   }
